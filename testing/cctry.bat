6633a5044a0362fbe6b227d7f5bf939a361492d7
Colin Jenkins <op12no2@gmail.com>
2021-02-08 08:01:26 +0000

    cache 2.0 wip

--- testing/cctry.bat
+++ testing/cctry.bat
@@ -12,26 +12,32 @@ rem for completing bad tests faster. Cutechess has access to basic SPRT
 rem testing. Make sure to use whatever setting prints SPRT results every
 rem few games. I think its something like "-RatingInterval N". Andrew.
 
+rem [x,y] == [reject,accept] - reject if < x, accept if > y, keep going
+rem otherwise. so if y islarge and there is in fact a big gain, the test w
+rem ill finish quickly.rbut if y is large and the gain is small the test w
+rem ill continue for arvery long time / forever. the test till finish w
+rem hen the llr hits a llr bound. (see the cc output every -ratinginterval
+rem games) we acan ccept -ve x for simplifications  for example.
 del cctry.pgn
 
 set tc=tc=0/10+0.01
 
 set e1=-engine conf=coalface %tc
-set e2=-engine conf=200      %tc
+set e2=-engine conf=master   %tc
 
-set t=-tournament round-robin -games 10000
+set t=-tournament round-robin -games 100000
 
 set r=-resign movecount=3 score=400
 
 set d=-draw movenumber=40 movecount=8 score=10
 
-set o=-repeat -openings file=4moves_noob.pgn format=pgn order=random plies=10
+set o=-repeat -srand %1 -openings file=4moves_noob.pgn format=pgn order=random plies=16
 
 set f=-pgnout cctry.pgn fi
 
 set s=-sprt elo0=0 elo1=5 alpha=0.05 beta=0.05 -ratinginterval 10
 
-set m=-recover -concurrency 2
+set m=-recover -concurrency 1
 
 "C:\Program Files (x86)\Cute Chess\cutechess-cli" %e1 %e2 %t %r %d %o %f %s %m
 
