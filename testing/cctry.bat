6046eb9f01648d1de33fbd2dcbf387e98cecab9a
Colin Jenkins <op12no2@gmail.com>
2021-02-10 11:35:19 +0000

    cache wip 2.0

--- testing/cctry.bat
+++ testing/cctry.bat
@@ -32,13 +32,13 @@ set r=-resign movecount=3 score=400
 
 set d=-draw movenumber=40 movecount=8 score=10
 
-set o=-repeat -srand %1 -openings file=4moves_noob.pgn format=pgn order=random plies=16
+set o=-repeat -srand %@Random[0,99999999] -openings file=4moves_noob.pgn format=pgn order=random plies=16
 
 set f=-pgnout cctry.pgn fi
 
 set s=-sprt elo0=0 elo1=5 alpha=0.05 beta=0.05 -ratinginterval 10
 
-set m=-recover -concurrency 2
+set m=-recover -concurrency 1
 
 "C:\Program Files (x86)\Cute Chess\cutechess-cli" %e1 %e2 %t %r %d %o %f %s %m
 
