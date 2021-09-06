075f5abd0f2033e0e0f3a4f43caf0505714c6539
Colin Jenkins <op12no2@gmail.com>
2021-09-06 09:58:36 +0100

    trivia

--- testing/cctry.bat
+++ testing/cctry.bat
@@ -1,10 +1,14 @@
 
+rem Use [ 0, 5] when testing a potential improvement.
+rem Use [ 0, 4] when tweaking an existing number, like a margin for pruning.
+rem Use [-3, 1] when simplifying some code that you think has little impact.
+
 del cctry.pgn
 
 set tc=tc=0/10+0.1
 
-set e1=-engine conf=coalface %tc
-set e2=-engine conf=released %tc
+set e1=-engine conf=coalface  %tc
+set e2=-engine conf=candidate %tc
 
 set t=-event soaktest -tournament round-robin -games 100000
 
@@ -18,11 +22,11 @@ set o=-repeat -srand %a -openings file=c:\projects\chessdata\4moves_noob.pgn for
 
 set f=-pgnout cctry.pgn fi
 
-set s=-sprt elo0=0 elo1=0 alpha=0.05 beta=0.05 -ratinginterval 10
+set s=-sprt elo0=0 elo1=5 alpha=0.05 beta=0.05
 
 set v=-ratinginterval 10
 
 set m=-recover -concurrency 4
 
-"C:\Program Files (x86)\Cute Chess\cutechess-cli" %e1 %e2 %t %r %d %o %f %v %m
+"C:\Program Files (x86)\Cute Chess\cutechess-cli" %e1 %e2 %t %r %d %o %f %v %m %s
 
