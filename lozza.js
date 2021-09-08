3dfb64c5567e6612a8793881b4182fcc32323060
Colin Jenkins <op12no2@gmail.com>
2021-09-08 14:27:06 +0100

    generalise phase curve

--- lozza.js
+++ lozza.js
@@ -16,6 +16,7 @@ var BUILD = "3";
 //{{{  history
 /*
 
+12/07/21 Generalise phase curve.
 12/07/21 Add xray term to eval.
 18/06/21 Add cwtch term to eval.
 16/06/21 Add reach term to eval.
@@ -891,6 +892,8 @@ const xray_S         = [0,0,0,4,7,-6];
 const xray_E         = [0,0,0,3,-6,0];
 const misc_S         = [10];
 const misc_E         = [0];
+const phase_S        = [24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0];
+const phase_E        = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 
 //}}}
 //{{{  pst lists
@@ -4025,7 +4028,7 @@ lozBoard.prototype.evaluate = function (turn) {
   
   var uci         = this.lozza.uci;
   var b           = this.b;
-  var phase       = this.cleanPhase(this.phase);
+  var phase       = this.cleanPhase(this.phase) | 0;
   var numPieces   = this.wCount + this.bCount;
   
   var wNumQueens  = this.wCounts[QUEEN];
@@ -4972,7 +4975,7 @@ lozBoard.prototype.evaluate = function (turn) {
   var evalS = materialS + positionS + imbalanceS + mobilityS + reachS + cwtchS + xrayS + biasS;
   var evalE = materialE + positionE + imbalanceE + mobilityE + reachE + cwtchE + xrayE + biasE;
   
-  var e = (evalS * (TPHASE - phase) + evalE * phase) / TPHASE;
+  var e = (evalS * phase_S[phase] + evalE * phase_E[phase]) / TPHASE;
   
   e = myround(e) | 0;
   
